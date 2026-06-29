c7757abae778c9afc8649ff6bbde417f0f275129
Adam Zaatar <Adam.Zaatar@progressoft.io>
2026-06-29 16:51:01 +0300

    feat: add rejected payment entry model

---
+++ eventguard-core/src/test/java/com/azaatar/eventguard/domain/RejectedPaymentEntryTest.java
@@ -0,0 +1,12 @@
+package com.azaatar.eventguard.domain;
+
+class RejectedPaymentEntryTest {
+    /*
+     * Tests to implement:
+     * givenValidFieldsWhenCreatingEntryThenStoresValues
+     * givenNullRawLineWhenCreatingEntryThenRejects
+     * givenBlankRawLineWhenCreatingEntryThenRejects
+     * givenNullRejectionReasonWhenCreatingEntryThenRejects
+     * givenOptionalFieldsAreNullWhenCreatingEntryThenStoresEntry
+     */
+}
